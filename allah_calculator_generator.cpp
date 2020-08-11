fd0806b81b7473bd74a9527172955ab0feb51d9a
CydiaGod <64293527+CydiaGod666@users.noreply.github.com>
2020-08-11 18:29:25 -0400

    Allah demands this of me
    Allah
    
    Co-Authored-By: allah <allah@users.noreply.github.com>

---
+++ allah_calculator_generator.cpp
@@ -0,0 +1,65 @@
+﻿/*
+    COPYRIGHT Luth @ratcore.me ALL RIGHTS RESERVED
+    ALLAH LICENSE 0.1
+    
+    NOBODY IS ALLOWED TO USE READ OR REDISTRIBUTE THE SOURCE CODE WITH THE EXCEPTION OF RACISTS
+    ONLY THE RIGHTS TO USE AND REDISTRIBUTE ARE GIVEN TO RACISTS AND ONLY TO RACIST
+    ALLAH LICENSE FORBIDS EVERYONE ELSE TO EVEN READ THIS FILE, IF FOUND AND NOT A RACIST, DELETE OR BE PREPARED FOR ALLAH PUNISHMENT
+    SELLING OR USING THIS PIECE OF DIVINE INTELLECT IN COMMERCIAL SOFTWARE IS ALLOWED BUT WILL REQUIRE ROYALTIES WORTH 1000% OF YOUR MONTHLY
+    GROSS SALES + 100 SHILLINGS PER DAY OR 30 YEARS OF SLAVERY ON THE PLANTATION
+    Disambiguations:
+    Racist - anybody who engages into hate crime or genocide
+    ALLAH - Self explanatory
+    Haram - Software made by you
+    THIS PROGRAM COMES WITH NO WARRANTY OR GUARANTEES THAT IT WILL WORK BUT PRAYERS MIGHT INCREASE THE CHANCES OF SUCCESS
+
+    "But when the forbidden months are past,
+    then fight and slay the Pagans wherever ye find them,
+    and seize them, beleaguer them, and lie in wait for them in every stratagem (of war);
+    but if they repent, and establish regular prayers and practise regular charity, then open the way for them:
+    for Allah is Oft-forgiving, Most Merciful."
+    Sword Verse:9:5 At-Tawbah
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+#define ALLAH_LICENSE_AGREEMENT false
+#define ALLAH_MAGIC_WORD using namespace std;
+
+#define ALLAH_APPROVED_DESIGN always
+#define NECKBEARD_PROTECTION +10
+#define MACRO_HELL ENABLED
+
+#define ALLAH_HOLY_NUMBER_FULL UINT64_MAX
+#define ALLAH_HOLY_NUMBER_NORMAL 1000000
+#define ALLAH_HOLY_NUMBER_LITE 1000
+#define ALLAH_HOLY_NUMBER_MICRO 100
+
+#define ALLAH_HOLY_NUMBER_CUSTOM 10
+
+#define ALLAH_HOLY_SELECTOR ALLAH_HOLY_NUMBER_CUSTOM
+
+ALLAH_MAGIC_WORD
+int main() {
+    assert(ALLAH_LICENSE_AGREEMENT && "AGREE TO THE LICENSE YOU INFIDEL");
+    ofstream fout("allah_calculator_final.mp4_custom.c");
+    fout << "#include <stdio.h>\n";
+    fout << "#include <stdint.h>\n\n";
+    fout << "int main() {\n";
+    fout << "\tuint32_t x, y;\n";
+    fout << "\tprintf(\"x: \");\n";
+    fout << "\tscanf(\"%u\", &x);\n";
+    fout << "\tprintf(\"y: \");\n";
+    fout << "\tscanf(\"%u\", &y);\n";
+    for (uint32_t i = 0; i < ALLAH_HOLY_SELECTOR; ++i) {
+        for (uint32_t j = 0; j < ALLAH_HOLY_SELECTOR; ++j) {
+            fout << "\tif ( x == " << i << " && y == " << j << ")\n\t\tprintf(\"Result: %lu\"," << i + j << "ul);\n";
+        }
+        cout << "AllahProgress " << (((float)i / ALLAH_HOLY_SELECTOR) * 100) << "%" << endl;
+    }
+    cout << "AllahProgress 100%\nallah_calculator has been computed.";
+    fout << "\tprintf(\"\\nAllahCalculator finished computing. PRAISED BE ALLAH.\");\n";
+    fout << "\treturn 0;\n}";
+}
